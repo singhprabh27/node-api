0f07652476b562db808730c23f8462a03014909d
Prabhjot <singhprabh2738@gmail.com>
2024-04-19 01:39:11 -0700

    changes host information from local to render

--- server.js
+++ server.js
@@ -21,11 +21,15 @@ app.use(cors());
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+        host: process.env.DATABASE_URL,
         port: 5432,
-        user: 'postgres',
-        password: 'test',
-        database: 'event_resource',
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PW,
+        database: process.env.DATABASE_DB,
     },
 });
 
